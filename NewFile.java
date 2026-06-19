4d31f0dbaae8335bed9b9e9232349d6ee2a4e2cb
Shahir Roswadi <shahirwadi@gmail.com>
2026-06-19 16:59:20 +0800

    Excercise 2

--- NewFile.java
+++ NewFile.java
@@ -1,5 +1,22 @@
+import com.fullstack.demo.Course;
+import com.fullstack.demo.Instructor;
+
 public class NewFile {
     public static void main(String[] args) {
+
+        Instructor instructor1 =
+                new Instructor("I001", "Shahir", "Java");
+
+        Course course1 = new Course(
+                "A1",
+                "Java Programming",
+                40,
+                "Beginner",
+                "Programming",
+                true
+        );
         
+        course1.setInstructor(instructor1);
+        course1.printSummary();
     }
 }
