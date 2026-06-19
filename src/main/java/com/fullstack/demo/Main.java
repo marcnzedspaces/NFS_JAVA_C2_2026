52c46519a5a1c7d754d1529e68bd7a434a0d3378
Shahir Roswadi <shahirwadi@gmail.com>
2026-06-19 17:40:06 +0800

    Exercise 3

--- src/main/java/com/fullstack/demo/Main.java
+++ src/main/java/com/fullstack/demo/Main.java
@@ -9,8 +9,8 @@ public class Main {
         Instructor instructor1 = new Instructor("I001", "Alice Johnson", "Java Development");
         Instructor instructor2 = new Instructor("I002", "Bob Smith", "React Development");
 
-        Course course1 = new Course("C001", "Java Fundamentals", 14, "Beginner");
-        Course course2 = new Course("C002", "React Frontend Development", 21, "Intermediate");
+        Course course1 = new Course("C001", "Java Fundamentals", 14, "Beginner", "Programming", true);
+        Course course2 = new Course("C002", "React Frontend Development", 21, "Intermediate", "Project", true);
 
         Student student1 = new Student("S001", "Charlie Brown", "cFq0l@example.com");
         Student student2 = new Student("S002", "Daisy Duck", "d4oQG@example.com");
@@ -30,5 +30,31 @@ public class Main {
         student1.printProfile();
         student2.printProfile();
 
+        Course javaCourse = new Course("C001", "Java Fundamentals", 2, "Beginner", "Programming", true);
+        Instructor instructor = new Instructor("I001", "Aina Rahman", "Java and Spring Boot");
+
+        CourseOffering offering = new CourseOffering(
+            "OFF001",
+            "Java Fundamentals - June 2026 Intake",
+            javaCourse,
+            instructor,
+            "2026-06-19",
+            "2026-06-20",
+            25,
+            "Online"
+        );
+            CourseOffering offering2 = new CourseOffering(
+            "OFF002",
+            "Java Fundamentals - July 2026 Intake",
+            javaCourse,
+            instructor,
+            "2026-07-19",
+            "2026-07-20",
+            20,
+            "Online"
+        );
+
+        offering.printOfferingSummary();
+        offering2.printOfferingSummary();
     }
 }
